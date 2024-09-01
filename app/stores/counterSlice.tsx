15b2f47a83ff3ca0922c601bb4c6df794aee7181
Georgi <g.ivanov@reply.com>
2024-09-01 21:05:29 +0100

    fix: state type name

--- app/stores/counterSlice.tsx
+++ app/stores/counterSlice.tsx
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 
-interface BearState {
+interface CounterState {
   count: number;
   increase: (by: number) => void;
   decrease: (by: number) => void;
@@ -8,7 +8,7 @@ interface BearState {
 // Consider using immer for nested states
 // https://github.com/pmndrs/zustand?tab=readme-ov-file#sick-of-reducers-and-changing-nested-states-use-immer
 
-export const useCounterStore = create<BearState>()((set) => ({
+export const useCounterStore = create<CounterState>()((set) => ({
   count: 0,
   increase: (by) => set((state) => ({ count: state.count + by })),
   decrease: (by) => set((state) => ({ count: state.count - by })),
