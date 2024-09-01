88b1ae55abe6ecc059f50873f9326d78bfbe9aff
Georgi <g.ivanov@reply.com>
2024-09-01 21:03:02 +0100

    refactor: redux for zustand

--- app/routes/counter.tsx
+++ app/routes/counter.tsx
@@ -1,32 +1,25 @@
-import type { RootState } from '../../app/store'
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from '../stores/counterSlice'
 import Button from '@mui/material/Button';
+import { useCounterStore } from '~/stores/counterSlice';
 
 export default function Counter() {
-  const count = useSelector((state: RootState) => state.counter.value)
-  const dispatch = useDispatch()
+  // Another option is to import the whole store and use it like this:
+  // const countStore = useCounterStore(); -> and then countStore.count, countStore.increase(), countStore.decrease()
+  // but bear in mind that it will cause the component to update on every state change!
+  // https://github.com/pmndrs/zustand?tab=readme-ov-file#fetching-everything
+  const { count, increase, decrease } = useCounterStore();
 
   return (
-    <div className='w-100'>
+    <div className="w-100">
       <p className="p-5 text-xl"> You are now in /counter </p>
-      <div className='w-100 flex space-x-10'>
-        <Button
-          variant="outlined"
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
+      <div className="w-100 flex space-x-10">
+        <Button variant="outlined" aria-label="Increment value" onClick={() => increase(1)}>
           Increment
         </Button>
         <span data-testid="count">{count}</span>
-        <Button
-          variant='outlined'
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
+        <Button variant="outlined" aria-label="Decrement value" onClick={() => decrease(1)}>
           Decrement
         </Button>
       </div>
     </div>
-  )
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vite";
+import { vitePlugin as remix } from '@remix-run/dev';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
     !process.env.VITEST &&
-    remix({
-      ssr: false,
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
+      remix({
+        ssr: false,
+        future: {
+          v3_fetcherPersist: true,
+          v3_relativeSplatPath: true,
+          v3_throwAbortReason: true,
+        },
+        serverModuleFormat: 'cjs',
+      }),
     tsconfigPaths(),
   ],
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: 'jsdom',
   },
 });
